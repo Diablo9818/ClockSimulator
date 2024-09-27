2e12e425dffab436ff5ef6b796b850a1aba5af83
Alexander Eroshchenko <diablo9818@gmail.com>
2024-09-27 19:50:32 +0200

    0.00

---
+++ Assets/Scripts/UI/Clock/DigitalClockView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Clock
+{
+    public class DigitalClockView : MonoBehaviour
+    {
+        [SerializeField] private Text _timeText;
+
+        private ClockViewModel _clockViewModel;
+
+        public void Init(ClockViewModel viewModel)
+        {
+            _clockViewModel = viewModel;
+
+            _clockViewModel.Date.OnChangeEvent += OnChangeTime;
+        }
+
+        private void OnChangeTime(DateTimeOffset date)
+        {
+            _timeText.text = string.Format("{0 : 00}:{1 : 00}:{2 : 00}", 
+                date.Hour, date.Minute, date.Second);
+        }
+
+        private void OnDestroy()
+        {
+            _clockViewModel.Date.OnChangeEvent -= OnChangeTime;
+        }
+    }
+}
